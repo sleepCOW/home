af204de9a92e3cb8731f4c2b902032690609547e
sleepCOW <olexthelake@gmail.com>
2025-02-12 00:31:04 +0200

    Fixed SignatureCreator.py GoTo and Define when multiple declarations present

--- saved/10XEditor/PythonScripts/SignatureCreator.py
+++ saved/10XEditor/PythonScripts/SignatureCreator.py
@@ -1,14 +1,23 @@
 import N10X
 
+def _IsFunctionDeclaration(Type: str) -> bool:
+    return Type == "MemberFunctionDeclaration" or Type == "FunctionDeclaration"
+
+
+def _IsFunctionDefinition(Type: str) -> bool:
+    return Type == "MemberFunctionDefinition" or Type == "FunctionDefinition"
+
+
 def _IsCurrentLineFunctionDeclaration():
-	CurrentLineText = N10X.Editor.GetCurrentLine()
-	X, Y = N10X.Editor.GetCursorPos()
-	
-	for i in range(0, len(CurrentLineText)):
-		SymbolType = N10X.Editor.GetSymbolType((i, Y))
-		if SymbolType == "MemberFunctionDeclaration" or SymbolType == "FunctionDeclaration":
-			return True
-	return False
+    CurrentLineText = N10X.Editor.GetCurrentLine()
+    X, Y = N10X.Editor.GetCursorPos()
+
+    for i in range(0, len(CurrentLineText)):
+        SymbolType = N10X.Editor.GetSymbolType((i, Y))
+        if _IsFunctionDeclaration(SymbolType):
+            return True
+    return False
+
 
 def __MakeImplementationSignature__(DeclarationLine: str):
     FunctionArgStart = DeclarationLine.find("(")
@@ -38,13 +47,13 @@ def __MakeImplementationSignature__(DeclarationLine: str):
     X, Line = N10X.Editor.GetCursorPos()
     Index = FunctionArgStart + 1
     for Arg in Args:
-    	ArgLen = len(Arg)
-    	for i in range(0, ArgLen):
-    		if N10X.Editor.GetSymbolType((Index + i, Line)) == "FunctionArg":
-    			FilteredArgs.append(Arg)
-    			break
-    	Index += 1 # Account for skipped ',' due to split()
-    	Index += ArgLen
+        ArgLen = len(Arg)
+        for i in range(0, ArgLen):
+            if N10X.Editor.GetSymbolType((Index + i, Line)) == "FunctionArg":
+                FilteredArgs.append(Arg)
+                break
+        Index += 1  # Account for skipped ',' due to split()
+        Index += ArgLen
 
     # Stupid python that doesn't allow modification in place...
     # So I need to write a bs one liner that is unreadable
@@ -58,71 +67,78 @@ def __MakeImplementationSignature__(DeclarationLine: str):
 
     return ReturnType + ScopeName + FunctionName + "(" + FilteredArgs + ")" + FunctionTrailingSpecifiers
 
+
+# Returns line number (starts counting from 0) and actual string where line is found
+# So the real line number is return + 1
+# But you want to do any API calls to 10x you need line number that starts counting from zero (returned one)
+# So if you want a real line number you need to +1
 def _FindLineNumber(LineToFind: str, Text: str):
-	Index = -1
+    i = 0
+    for Line in Text.splitlines():
+        if LineToFind in Line:
+            return i, Line
+        i = i + 1
 
-	i = 0
-	for Line in Text.splitlines():
-		if LineToFind == Line:
-			return i
-		i = i + 1
+    return -1, None
 
-	return Index
 
-# Searches currently selected line for MemberFunctionDeclaration or FunctionDeclaration
-# And executes GotoSymbolDefinition for the found declaration
-def _StupidGoTo():
-	CurrentLineText = N10X.Editor.GetCurrentLine()
-	X, Y = N10X.Editor.GetCursorPos()
-	
-	for i in range(0, len(CurrentLineText)):
-		SymbolType = N10X.Editor.GetSymbolType((i, Y))
-		if SymbolType == "MemberFunctionDeclaration" or SymbolType == "FunctionDeclaration":
-			N10X.Editor.ExecuteCommand("GotoSymbolDefinition")
-			break
+def _Define(bToggleSourceHeader: bool):
+    if not _IsCurrentLineFunctionDeclaration():
+        print("Function already defined")
+        return
 
-def _Define(bToggleSourceHeader : bool):
-	if not _IsCurrentLineFunctionDeclaration():
-		return
+    CurrentLineText = N10X.Editor.GetCurrentLine()
 
-	ClassName = N10X.Editor.GetCurrentScopeName()
-	CurrentLineText = N10X.Editor.GetCurrentLine()
+    Signature = __MakeImplementationSignature__(CurrentLineText)
+    SourceToPaste = Signature + "\n{\n\n}"
 
-	Signature = __MakeImplementationSignature__(CurrentLineText)
-	SourceToPaste = Signature + "\n{\n\n}"
+    if bToggleSourceHeader:
+        N10X.Editor.ExecuteCommand("CppParser.ToggleSourceHeader")
 
-	if bToggleSourceHeader:
-		N10X.Editor.ExecuteCommand("CppParser.ToggleSourceHeader")
-	
-	PageText = N10X.Editor.GetFileText()
+    PageText: str = N10X.Editor.GetFileText()
 
-	# TODO: Make sure found text is an actual FunctionDefinition
-	if Signature in PageText:
-		# If we found a signature find the line where signature is located, so we can do GoTo
-		print("Function " + Signature + " already defined")
-		_StupidGoTo()
-		return
+    # Check whether function is already defined in the file
+    # Remember we could have multiple declarations in the file, so we need to check all of them
+    SearchPageTextSlice = PageText
+    AccumulatedLineNum = 0
+    while True:
+        SignatureIndex = SearchPageTextSlice.find(Signature)
 
-	# We can't simply use _StupidGoTo because we can be in another file
-	# Attempt to hack using ToggleSourceHeader and StupidGoTo had no success
+        # No more signatures in text
+        if SignatureIndex == -1:
+            break
 
-	# TODO: Function must be pasted between previous and next function, so order is maintained
-	PageText += "\n\n" + SourceToPaste
-	N10X.Editor.SetFileText(PageText)
-	
-	LineNum = _FindLineNumber(Signature, PageText)
-	# Set position to be in the curly brackets so we can start typing right away
-	N10X.Editor.SetCursorPos((4, LineNum + 2))
+        LineNum, Line = _FindLineNumber(Signature, SearchPageTextSlice)
+        AccumulatedLineNum += LineNum
+        for X in range(0, len(Line)):
+            SymType = N10X.Editor.GetSymbolType((X, AccumulatedLineNum))
+            if _IsFunctionDefinition(SymType):
+                print("Function " + Signature + " already defined")
+                N10X.Editor.SetCursorPos((X, AccumulatedLineNum))
+                return
+
+        SignatureIndex += len(Line)
+        SearchPageTextSlice = SearchPageTextSlice[SignatureIndex:]
+
+    # TODO: Function must be pasted between previous and next function, so order is maintained
+    PageText += "\n\n" + SourceToPaste
+    N10X.Editor.SetFileText(PageText)
+
+    LineNum, Line = _FindLineNumber(Signature, PageText)
+    # Set position to be in the curly brackets so we can start typing right away
+    N10X.Editor.SetCursorPos((4, LineNum + 2))
+
 
 def Define():
-	FileName = N10X.Editor.GetCurrentFilename()
-	Extension = FileName.split('.')[-1]
+    FileName = N10X.Editor.GetCurrentFilename()
+    Extension = FileName.split('.')[-1]
 
-	# If happen to call define when we are in "Source" file we defenetily want it to be defined in current file, so no Toggle Source/Header
-	if Extension in ["cpp", "cxx", "c"]:
-		_Define(False)
-	else:
-		_Define(True)
+    # If we happen to call define when we are in "Source" file we defenetily want it to be defined in current file, so no Toggle Source/Header
+    if Extension in ["cpp", "cxx", "c"]:
+        _Define(False)
+    else:
+        _Define(True)
+
 
 def DefineInCurrentFile():
-	_Define(False)
+    _Define(False)
