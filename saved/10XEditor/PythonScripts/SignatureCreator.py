21cf251b5171a118c7eb103a6fc9b51a7668ab4b
sleepCOW <olexthelake@gmail.com>
2025-02-11 22:18:54 +0200

    Improvement for SignatureCreator.py * DefineInCurrentFile * Define tracks the file from where it's been called, so when defining in Source file no header/source toggle * Fixes for complex default arguments

--- saved/10XEditor/PythonScripts/SignatureCreator.py
+++ saved/10XEditor/PythonScripts/SignatureCreator.py
@@ -1,13 +1,18 @@
 import N10X
 
+def _IsCurrentLineFunctionDeclaration():
+	CurrentLineText = N10X.Editor.GetCurrentLine()
+	X, Y = N10X.Editor.GetCursorPos()
+	
+	for i in range(0, len(CurrentLineText)):
+		SymbolType = N10X.Editor.GetSymbolType((i, Y))
+		if SymbolType == "MemberFunctionDeclaration" or SymbolType == "FunctionDeclaration":
+			return True
+	return False
+
 def __MakeImplementationSignature__(DeclarationLine: str):
     FunctionArgStart = DeclarationLine.find("(")
     FunctionArgEnd = DeclarationLine.rfind(")")
-
-    # error not a function if couldn't find any () in the line
-
-    FunctionNameStart = DeclarationLine[0:FunctionArgStart].rfind(" ")
-
     FunctionName = DeclarationLine[0:FunctionArgStart].split()[-1]
 
     ReturnTypeEnd = DeclarationLine.find(FunctionName)
@@ -26,17 +31,32 @@ def __MakeImplementationSignature__(DeclarationLine: str):
     # Extract function arguments from selected line
     Args = DeclarationLine[FunctionArgStart + 1:FunctionArgEnd].split(',')
 
+    # To allow complex default values like const FVector InVec = FVector( 2.f, 4.f )
+    # We need to verify that each splitted part has FunctionArg symbol in it
+    # And I actually need to go through indexes rather array of tokens in Args because SymbolType operates based on CursorPos
+    FilteredArgs = []
+    X, Line = N10X.Editor.GetCursorPos()
+    Index = FunctionArgStart + 1
+    for Arg in Args:
+    	ArgLen = len(Arg)
+    	for i in range(0, ArgLen):
+    		if N10X.Editor.GetSymbolType((Index + i, Line)) == "FunctionArg":
+    			FilteredArgs.append(Arg)
+    			break
+    	Index += 1 # Account for skipped ',' due to split()
+    	Index += ArgLen
+
     # Stupid python that doesn't allow modification in place...
     # So I need to write a bs one liner that is unreadable
     # Remove any default params from arguments and any excessive whitespaces that left.
-    Args = [Arg.split("=")[0].rstrip() for Arg in Args]
+    FilteredArgs = [Arg.split("=")[0].rstrip() for Arg in FilteredArgs]
 
     Delimeter = ','
-    Args = Delimeter.join(Args)
+    FilteredArgs = Delimeter.join(FilteredArgs)
 
     ScopeName = N10X.Editor.GetCurrentScopeName() + "::" if N10X.Editor.GetCurrentScopeName() else ""
 
-    return ReturnType + ScopeName + FunctionName + "(" + Args + ")" + FunctionTrailingSpecifiers
+    return ReturnType + ScopeName + FunctionName + "(" + FilteredArgs + ")" + FunctionTrailingSpecifiers
 
 def _FindLineNumber(LineToFind: str, Text: str):
 	Index = -1
@@ -49,117 +69,60 @@ def _FindLineNumber(LineToFind: str, Text: str):
 
 	return Index
 
-# In case user didn't
+# Searches currently selected line for MemberFunctionDeclaration or FunctionDeclaration
+# And executes GotoSymbolDefinition for the found declaration
 def _StupidGoTo():
 	CurrentLineText = N10X.Editor.GetCurrentLine()
 	X, Y = N10X.Editor.GetCursorPos()
 	
 	for i in range(0, len(CurrentLineText)):
 		SymbolType = N10X.Editor.GetSymbolType((i, Y))
-		if SymbolType == "MemberFunctionDeclaration":
+		if SymbolType == "MemberFunctionDeclaration" or SymbolType == "FunctionDeclaration":
 			N10X.Editor.ExecuteCommand("GotoSymbolDefinition")
 			break
 
-def _Define(bDefineInSourceFile : bool):
+def _Define(bToggleSourceHeader : bool):
+	if not _IsCurrentLineFunctionDeclaration():
+		return
+
 	ClassName = N10X.Editor.GetCurrentScopeName()
 	CurrentLineText = N10X.Editor.GetCurrentLine()
 
 	Signature = __MakeImplementationSignature__(CurrentLineText)
 	SourceToPaste = Signature + "\n{\n\n}"
 
-	if bDefineInSourceFile:
+	if bToggleSourceHeader:
 		N10X.Editor.ExecuteCommand("CppParser.ToggleSourceHeader")
 	
 	PageText = N10X.Editor.GetFileText()
 
+	# TODO: Make sure found text is an actual FunctionDefinition
 	if Signature in PageText:
 		# If we found a signature find the line where signature is located, so we can do GoTo
 		print("Function " + Signature + " already defined")
 		_StupidGoTo()
-		#LineNum = _FindLineNumber(Signature, PageText)
-		#N10X.Editor.SetCursorY(LineNum)
 		return
 
-	# We can't simply use _StupidGoTo because we maybe in another file
+	# We can't simply use _StupidGoTo because we can be in another file
 	# Attempt to hack using ToggleSourceHeader and StupidGoTo had no success
 
+	# TODO: Function must be pasted between previous and next function, so order is maintained
 	PageText += "\n\n" + SourceToPaste
 	N10X.Editor.SetFileText(PageText)
 	
-	# Can simply split and count PageText lines
 	LineNum = _FindLineNumber(Signature, PageText)
 	# Set position to be in the curly brackets so we can start typing right away
 	N10X.Editor.SetCursorPos((4, LineNum + 2))
 
 def Define():
-	_Define(True)
+	FileName = N10X.Editor.GetCurrentFilename()
+	Extension = FileName.split('.')[-1]
 
-def DefineInHeader():
+	# If happen to call define when we are in "Source" file we defenetily want it to be defined in current file, so no Toggle Source/Header
+	if Extension in ["cpp", "cxx", "c"]:
+		_Define(False)
+	else:
+		_Define(True)
+
+def DefineInCurrentFile():
 	_Define(False)
-
-def TestCom():
-	print(N10X.Editor.GetFileText())
-	MousePos = N10X.Editor.GetCursorPos()
-	print(MousePos)
-	print(N10X.Editor.GetLine(MousePos[1]))
-
-
-def SymbolType():
-	MousePos = N10X.Editor.GetCursorPos()
-	print(N10X.Editor.GetSymbolType(MousePos))
-
-def SymbolDefinition():
-	MousePos = N10X.Editor.GetCursorPos()
-	print(N10X.Editor.GetSymbolDefinition(MousePos))
-
-def OldDefine():
-	ClassName = N10X.Editor.GetCurrentScopeName()
-	CurrentLineText = N10X.Editor.GetCurrentLine()
-
-	BeforeVariables = CurrentLineText.split("(")[0]
-
-	print(ClassName)
-	print(CurrentLineText)
-	print(BeforeVariables)
-    #print(BeforeVariables)
-
-	#removing a stupid amount of edge cases
-	BeforeVariables = BeforeVariables.replace(";","")
-	BeforeVariables = BeforeVariables.replace("virtual", "")
-	BeforeVariables = BeforeVariables.replace("override", "")
-	BeforeVariables = BeforeVariables.replace("const", "")
-	BeforeVariables = BeforeVariables.replace("static", "")
-
-	CurrentLineText = BeforeVariables + "(" + CurrentLineText.split("(")[1]
-	CurrentLineText = CurrentLineText.replace(';', '')
-
-
-	SplitLine = CurrentLineText.split()
-
-	RestOfTheDefinition = ""
-	
-	for i in range(len(SplitLine)):
-		if i == 0:
-			continue
-		if i == 1:
-			RestOfTheDefinition += SplitLine[i]
-		else:
-			RestOfTheDefinition += " " + SplitLine[i]
-
-	FinalDefinitionNoCurlies = SplitLine[0] + " " + ClassName + "::" + RestOfTheDefinition
-	FinalDefinition = FinalDefinitionNoCurlies + "\n{\n\n}"
-	ToggleCppHeader()
-	
-	PageText = N10X.Editor.GetFileText()
-	
-	if FinalDefinitionNoCurlies in PageText:
-		print("Function " + FinalDefinitionNoCurlies + " already defined")
-		ToggleCppHeader()
-		return
-
-	PageText += "\n\n" + FinalDefinition
-	N10X.Editor.SetFileText(PageText)
-
-def ToggleCppHeader():
-	FileName = N10X.Editor.ExecuteCommand("CppParser.ToggleSourceHeader")
-
